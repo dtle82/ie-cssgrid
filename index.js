5eeb03bcbe3d8fd5f4491ac4ba4d3cc22e1411d5
Danh Le <dtle82@gmail.com>
2020-05-04 20:04:53 -0700

    added lorem ipsum

--- index.js
+++ index.js
@@ -5,17 +5,3 @@ var animation = bodymovin.loadAnimation({
   autoplay: true,
   path: 'lottie.json'
 })
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('bm2'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'lottie.json'
-})
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('bm3'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'lottie.json'
-})

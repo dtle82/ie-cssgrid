722be48db0a5258a6beac47f080bda7c0d9b5ba9
Danh Le <dtle82@gmail.com>
2020-05-04 20:01:52 -0700

    added multiple lotties

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var animation = bodymovin.loadAnimation({
-  container: document.getElementById('bm'),
+  container: document.getElementById('bm1'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'lottie.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'lottie.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm3'),
   renderer: 'svg',
   loop: true,
   autoplay: true,
